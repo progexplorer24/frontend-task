645c2ed60b75726c3899abce0f697fa0e5a0a8f4
Progexplorer <progexplorer@gmail.com>
2022-05-26 09:32:14 +0200

    refactor: form submit icon

--- src/components/FormSubmit.tsx
+++ src/components/FormSubmit.tsx
@@ -1,22 +1,23 @@
 import React, { FC } from 'react';
 import './FormSubmit.scss';
 interface FormSubmitProps {
-  text: string
+  text: string,
 }
 
+const saveIcon = <svg xmlns="http://www.w3.org/2000/svg" className="save" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+<path strokeLinecap="round" strokeLinejoin="round" d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
+</svg>
+
 const FormSubmit: FC<FormSubmitProps> = ({text}) => {
+
   return (
     <button type="submit" className="FormBtn">
     <span className="svg-wrapper">
-    <svg xmlns="http://www.w3.org/2000/svg" className="save" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-  <path strokeLinecap="round" strokeLinejoin="round" d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
-</svg>
+    {saveIcon}
     </span>
     {text}
   </button>
   );
 };
-
-
 
 export default FormSubmit;
