4e8739e69326320a5963841e006cfbb644632645
Progexplorer <progexplorer@gmail.com>
2022-05-26 19:22:44 +0200

    feat: improve filtering strategy

--- src/components/FilterForm.tsx
+++ src/components/FilterForm.tsx
@@ -5,69 +5,53 @@ import type {ColorParsed} from "../utils/predefined-colors"
 import ColorList from "./ColorList"
 interface FilterFormProps {
   filteredColors: ColorParsed[]
+  colors: ColorParsed[]
   setColors: React.Dispatch<React.SetStateAction<ColorParsed[]>>
   setFilteredColors: React.Dispatch<React.SetStateAction<ColorParsed[]>>
 }
 
 
-type RemovedColors = {
-  red: ColorParsed[],
-  green: ColorParsed[],
-  blue: ColorParsed[],
-  saturation: ColorParsed[]
-}
+const FilterForm: FC<FilterFormProps> = ({setFilteredColors, setColors, filteredColors, colors}) => {
+  const [isRedFiltered, setIsRedFiltered] = useState(false);
+  const [isGreenFiltered, setIsGreenFiltered] = useState(false);
+  const [isBlueFiltered, setIsBlueFiltered] = useState(false);
+  const [isSaturationFiltered, setIsSaturationFiltered] = useState(false);
 
-const FilterForm: FC<FilterFormProps> = ({setFilteredColors, setColors, filteredColors}) => {
-  const [removedColors, setRemovedColors] = useState<RemovedColors>({
-    red: [],
-    green: [],
-    blue: [],
-    saturation: []
-  }) 
 
     // Should every checkbox have different event handler
     const handleInputCheckbox = (event: React.FormEvent<HTMLInputElement>) => {
-      console.log(event.currentTarget)
       const {name} = event.currentTarget
       if (name === "red") {
-        if(event.currentTarget.checked) {
-          setRemovedColors(prev => {
-            return {...prev, red: filteredColors.filter((el: ColorParsed) => el.color[0] <= 127)}
-          })
-          setFilteredColors(prev => prev.filter(el => el.color[0] > 127))
-        } else {
-          setFilteredColors(prev => sortColorsArray([...removedColors.red, ...prev]))
-        }
+        const shouldFilterRed = !isRedFiltered ? colors.filter(el => el.color[0] > 127) : colors;
+        const shouldFilterGreen = isGreenFiltered ? shouldFilterRed.filter(el => el.color[1] > 127) : shouldFilterRed;
+        const shouldFilterBlue = isBlueFiltered ? shouldFilterGreen.filter(el => el.color[2] > 127) : shouldFilterGreen;
+        const shouldFilterSaturation = isSaturationFiltered ? shouldFilterBlue.filter(el => rgbToHsl(el.color[0], el.color[1], el.color[2])[1] > 50) : shouldFilterBlue;
+        setFilteredColors(sortColorsArray(shouldFilterSaturation))
+        setIsRedFiltered(!isRedFiltered)
   
       } else if (name === "green") {
-        if(event.currentTarget.checked) {
-          setRemovedColors(prev => {
-            return {...prev, green: filteredColors.filter((el: ColorParsed) => el.color[1] <= 127)}
-          })
-          setFilteredColors(prev => prev.filter(el => el.color[1] > 127))
-        } else {
-          setFilteredColors(prev => sortColorsArray([...removedColors.green, ...prev]))
-        }
+        const shouldFilterRed = isRedFiltered ? colors.filter(el => el.color[0] > 127) : colors;
+        const shouldFilterGreen = !isGreenFiltered ? shouldFilterRed.filter(el => el.color[1] > 127) : shouldFilterRed;
+        const shouldFilterBlue = isBlueFiltered ? shouldFilterGreen.filter(el => el.color[2] > 127) : shouldFilterGreen;
+        const shouldFilterSaturation = isSaturationFiltered ? shouldFilterBlue.filter(el => rgbToHsl(el.color[0], el.color[1], el.color[2])[1] > 50) : shouldFilterBlue;
+        setFilteredColors(sortColorsArray(shouldFilterSaturation))
+        setIsGreenFiltered(!isGreenFiltered)
   
   
       } else if (name === "blue") {
-        if(event.currentTarget.checked) {
-          setRemovedColors(prev => {
-            return {...prev, blue: filteredColors.filter((el: ColorParsed) => el.color[2] <= 127)}
-          })
-          setFilteredColors(prev => prev.filter(el => el.color[2] > 127))
-        } else {
-          setFilteredColors(prev => sortColorsArray([...removedColors.blue, ...prev]))
-        }
+        const shouldFilterRed = isRedFiltered ? colors.filter(el => el.color[0] > 127) : colors;
+        const shouldFilterGreen = isGreenFiltered ? shouldFilterRed.filter(el => el.color[1] > 127) : shouldFilterRed;
+        const shouldFilterBlue = !isBlueFiltered ? shouldFilterGreen.filter(el => el.color[2] > 127) : shouldFilterGreen;
+        const shouldFilterSaturation = isSaturationFiltered ? shouldFilterBlue.filter(el => rgbToHsl(el.color[0], el.color[1], el.color[2])[1] > 50) : shouldFilterBlue;
+        setFilteredColors(sortColorsArray(shouldFilterSaturation))
+        setIsBlueFiltered(!isBlueFiltered)
       } else if (name === "saturation") {
-        if(event.currentTarget.checked) {
-          setRemovedColors(prev => {
-            return {...prev, saturation: filteredColors.filter((el: ColorParsed) => rgbToHsl(el.color[0], el.color[1], el.color[2])[1] <= 50)}
-          })
-          setFilteredColors(prev => prev.filter(el => rgbToHsl(el.color[0], el.color[1], el.color[2])[1] > 50))
-        } else {
-          setFilteredColors(prev => sortColorsArray([...removedColors.saturation, ...prev]))
-        }
+        const shouldFilterRed = isRedFiltered ? colors.filter(el => el.color[0] > 127) : colors;
+        const shouldFilterGreen = isGreenFiltered ? shouldFilterRed.filter(el => el.color[1] > 127) : shouldFilterRed;
+        const shouldFilterBlue = isBlueFiltered ? shouldFilterGreen.filter(el => el.color[2] > 127) : shouldFilterGreen;
+        const shouldFilterSaturation = !isSaturationFiltered ? shouldFilterBlue.filter(el => rgbToHsl(el.color[0], el.color[1], el.color[2])[1] > 50) : shouldFilterBlue;
+        setFilteredColors(sortColorsArray(shouldFilterSaturation))
+        setIsSaturationFiltered(!isSaturationFiltered)
       } else {
         console.log("This statement should never run!")
       }
@@ -81,19 +65,19 @@ const FilterForm: FC<FilterFormProps> = ({setFilteredColors, setColors, filtered
     <p className="paragraph">Choose filtering conditions</p>
     <label className="checkbox-label" >
       Pick Red Values higher than 127:
-    <input type="checkbox" className="form-checkbox" name="red" onChange={handleInputCheckbox} />
+    <input type="checkbox" className="form-checkbox" name="red" checked={isRedFiltered} onChange={handleInputCheckbox} />
     </label>
     <label className="checkbox-label">
       Pick Green Values higher than 127:
-    <input type="checkbox" className="form-checkbox" name="green" onChange={handleInputCheckbox} />
+    <input type="checkbox" className="form-checkbox" name="green" checked={isGreenFiltered}  onChange={handleInputCheckbox} />
     </label>
     <label className="checkbox-label">
       Pick Blue Values higher than 127:
-    <input type="checkbox" className="form-checkbox" name="blue" onChange={handleInputCheckbox} />
+    <input type="checkbox" className="form-checkbox" name="blue" checked={isBlueFiltered}  onChange={handleInputCheckbox} />
     </label>
     <label className="checkbox-label">
       Pick Saturation Values over 50%:
-    <input type="checkbox" className="form-checkbox" name="saturation" onChange={handleInputCheckbox} />
+    <input type="checkbox" className="form-checkbox" name="saturation" checked={isSaturationFiltered}  onChange={handleInputCheckbox} />
     </label>
   </form>
   <ColorList colorList={filteredColors} setColors={setColors} setFilteredColors={setFilteredColors} />
